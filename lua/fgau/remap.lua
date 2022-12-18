83b428dbeab11aac9615b6e58a5cb75e988c4145
F. Gau <fgau@gau-net.de>
2022-12-18 23:18:09 +0100

    new keymaps

--- lua/fgau/remap.lua
+++ lua/fgau/remap.lua
@@ -1,2 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Navigate buffers
+vim.keymap.set("n", "<S-l>", ":bnext<CR>")
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>")
+
+-- Visual --
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
