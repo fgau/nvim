9074fc9185598c20ae6cfa97697b87337bc27a07
F. Gau <fgau@gau-net.de>
2023-05-24 22:43:00 +0200

    add isort command and beautify

--- lua/fgau/remap.lua
+++ lua/fgau/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Navigate buffers
 vim.keymap.set("n", "<S-l>", ":bnext<CR>")
