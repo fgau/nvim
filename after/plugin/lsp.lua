82706682e00b87ee2584fb9497a4bdbe79ba9b96
F. Gau <fgau@gau-net.de>
2023-05-27 18:59:43 +0200

    fix vim in lua diagnostics

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,11 +3,23 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
+    'lua_ls',
     'tsserver',
     'eslint',
     'pyright',
     'gopls',
     'marksman',
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('lua_ls', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
 })
 
 local cmp = require('cmp')
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "help", "javascript", "typescript", "go", "python", "html", "css", "json", "markdown", "c", "lua", "rust" },
-
+    -- ensure_installed = { "javascript", "typescript", "go", "python", "html", "css", "json", "markdown", "c", "lua", "rust" },
+    -- ensure_installed = { "all" },
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
 
