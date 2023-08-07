40691c4d66d8e9f0b27363f304420c8ca9ee561f
F. Gau <fgau@gau-net.de>
2023-08-07 17:04:44 +0200

    up and down

--- lua/fgau/remap.lua
+++ lua/fgau/remap.lua
@@ -10,6 +10,10 @@ vim.keymap.set("n", "<S-h>", ":bprevious<CR>")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
+-- move marked lines up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- netrw directoy listing
 vim.keymap.set("n", "<leader>n", ":Ex<CR>")
 
