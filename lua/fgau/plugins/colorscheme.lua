7d8db92e9ea00a619d5cae7ab554a0b434a3c183
fgau <fgau@gau-net.de>
2025-04-24 23:02:01 +0200

    add new plugin live-preview (markdown)

--- lua/fgau/plugins/colorscheme.lua
+++ lua/fgau/plugins/colorscheme.lua
@@ -12,6 +12,19 @@ return {
 	end,
 }
 -- return {
+-- 	"savq/melange-nvim",
+-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+-- 	priority = 1000, -- make sure to load this before all the other start plugins
+-- 	config = function()
+-- 		-- load the colorscheme here
+-- 		vim.cmd([[colorscheme melange]])
+--
+-- 		-- make it transparent
+-- 		vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+-- 		vim.cmd("hi EndOfBuffer guibg=NONE ctermbg=NONE")
+-- 	end,
+-- }
+-- return {
 -- 	"rose-pine/neovim",
 -- 	name = "rose-pine",
 -- 	variant = "moon",
--- lua/fgau/plugins/lualine.lua
+++ lua/fgau/plugins/lualine.lua
@@ -1,20 +1,27 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  opts = function()
-    return {
-      options = {
-        icons_enabled = true,
-        theme = 'gruvbox',
-      },
-      sections = {
-        lualine_a = {
-          {
-            'filename',
-            path = 1,
-          }
-        }
-      }
-    }
-  end,
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	opts = function()
+		return {
+			options = {
+				icons_enabled = true,
+				theme = "gruvbox",
+			},
+			sections = {
+				lualine_a = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+				lualine_x = {
+					{
+						"datetime",
+						-- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
+						style = "%H:%M",
+					},
+				},
+			},
+		}
+	end,
 }
