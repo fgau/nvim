114801ae9acb08e4a66ce85d5b3e54dd976c282e
F. Gau <fgau@gau-net.de>
2025-05-12 22:02:15 +0200

    more general options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,19 @@
 vim.opt.number = true
-vim.opt.cursorline = true
 vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+
+vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.signcolumn = "yes"
 
 vim.opt.list = true
 vim.opt.listchars = {
